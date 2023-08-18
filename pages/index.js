41f734fd103c05a2bb97c5668a0b33c9dddc2517
maiOSX <124741903+maiOSX@users.noreply.github.com>
2023-08-18 12:48:28 -0500

    added next and supabase and env variables

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>work in progress</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="nodoom" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is an upcoming app. 
         </p>
       </main>
 
